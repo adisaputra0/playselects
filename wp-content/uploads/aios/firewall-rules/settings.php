0bde041df6ad36150a4278e076146aa868fb94f4
putuadi208@gmail.com <putuadi208@gmail.com>
2023-12-25 13:53:44 +0800

    edit url admin, url categories, and add new user

---
+++ wp-content/uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"","aios_login_page_slug":"","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_6bac6853c2002e04dd4011bf40167b68","aios_brute_force_cookie_salt":"g$&ciF7&ui22%56\/2^D\/WTyx}bTa5V;qj}`3?WKu#MC]]?,f10Vb0t$F6r#7a^*Ud(_E-(>I,F|JaMxW?#H=5o%BL<66AfuUHAkZa3Cjl]{cbx,GMfxs \/W`WC(-~-~V","aios_ip_retrieve_method":0,"aiowps_ip_retrieve_method":"","aiowps_blacklist_ips":[],"aiowps_blacklist_user_agents":[]}
